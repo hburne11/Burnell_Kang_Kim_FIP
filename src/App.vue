c4ac03b92b58a4fe9f0358a295468ef1a464545a
yeonjuKim <y_kim113576@fanshaweonline.ca>
2022-03-07 14:19:35 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="container">
+    <div id="app">
+      <header>
+        <nav id="nav">
+          <router-link to="/">
+            <img src="@/assets/images/logo.png" />
+          </router-link>
+
+          <div class="menu">
+            <router-link to="/" class="is-active"> HOME </router-link>
+            <router-link to="/CampaignView"> OUR CAMPAIGN </router-link>
+            <router-link to="/AboutView">ABOUT US </router-link>
+            <router-link to="/ContactView"> CONTACT US </router-link>
+            <button class="donationBtn">DONATE</button>
+          </div>
+          <button class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </nav>
+      </header>
+      <div class="router">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/sass/_header.scss";
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
+
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+});
