f5296bb0ac4dc8efb187338d28c9a277c6745c34
YeonjuKim05 <y_kim113576@fanshaweonline.ca>
2022-03-23 21:55:39 +0900

    running app-wide version

---
+++ src/views/DonationView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    <div class="contactView">
+      <h1>This is a Contact page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptas
+        rerum ratione, ad molestiae nisi maxime commodi eius harum, sint
+        accusantium facilis beatae quos dicta, ipsum mollitia quibusdam eveniet
+        error.
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "donationView",
+};
+</script>
+
+<style lang="sass">
+@import "@/assets/sass/_donation.scss";
+</style>
