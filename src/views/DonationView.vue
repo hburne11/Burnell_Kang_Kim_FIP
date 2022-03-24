c111cb604dab39d146ad7ab5b11bbe1cff5c2bf4
YeonjuKim05 <y_kim113576@fanshaweonline.ca>
2022-03-25 05:08:55 +0900

    tweaked css in all pages

--- src/views/DonationView.vue
+++ src/views/DonationView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <div class="contactView">
-      <h1>This is a Contact page</h1>
+      <h1>This is a Donation page</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptas
         rerum ratione, ad molestiae nisi maxime commodi eius harum, sint
