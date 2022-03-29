d955b5c5a2f0602dfe5587cdbc8e06c47bde8ef7
YeonjuKim05 <y_kim113576@fanshaweonline.ca>
2022-03-29 20:52:48 +0900

    edited css gallery page

--- apopo-front/src/views/GalleryView.vue
+++ apopo-front/src/views/GalleryView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="main-gallery">
+    <h1>GALLERY</h1>
     <div class="gallery">
-      <h1>GALLERY</h1>
       <div class="image-wrapper">
         <GalleryImg
           v-for="photo in photos"
